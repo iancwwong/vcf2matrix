a9192c56f0fdfb2fb710dd9896ea0e016f115b7c
iancwwong <wong.iancw@gmail.com>
2017-05-05 17:39:17 +1000

    Documentation for converter

--- src/Converter.cpp
+++ src/Converter.cpp
@@ -8,8 +8,21 @@
 Converter::Converter() {};
 Converter::~Converter() {};
 
-/* Convert to ParsedSNP function */
+/* 
+	Convert to ParsedSNP function.
+	Follows the VCF 4.2 column format, with the following 8 (tab-delimitered, order-specific) columns:
+		CHROM	POS	ID	REF	ALT	QUAL	FILTER	INFO
+	If there are genotypes to follow, a FORMAT field is given specifying the data types 
+	and order (colon-separated alphanumeric String). Under this field, the first subfield (if there is any)
+	must ALWAYS be the genotype (GT). .
+
+	The full documentation of the VCF 4.2 standard can be found at:
+	https://samtools.github.io/hts-specs/VCFv4.2.pdf
+
+ */
 ParsedSNP * Converter::convert(string data, int alleleFreq, int confScore) {
-	
+
+	/* VCF 4.2 COLUMN FORMAT: #CHROM	POS	ID	REF	ALT	QUAL	FILTER	INFO	FORMAT	[Sample names] */
+
 	return NULL;
 }
